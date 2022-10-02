21cf5fc804d2c203ed853d5643adc7e088918db7
ThXilos <theodksylo@gmail.com>
2022-10-02 19:57:44 +0300

    add login functionality & Profile schema  setup

---
+++ models/Profile.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  status: {
+    type: String,
+    require: true,
+  },
+  bio: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+    require: true,
+  },
+  social: {
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
